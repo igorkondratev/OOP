768b42ffb78b38fcd95ffa6a8033a6ca265a3684
igorkondratev <71003957+igorkondratev@users.noreply.github.com>
2021-11-05 00:50:50 +0300

    Update mainfunc.h

--- lab3/lab3_dinamic/mainfunc.h
+++ lab3/lab3_dinamic/mainfunc.h
@@ -1,7 +1,9 @@
-
+#ifdef LAB_3_CLASS_H
+#define LAB_3_CLASS_H
 int dialog(const char* msgs[], int N);
 void retincor(Vector&V);
 void retand(Vector&V);
 void retiskili(Vector&V);
 void dop(Vector&V);
 void ogr(Vector&V);
+#endif
